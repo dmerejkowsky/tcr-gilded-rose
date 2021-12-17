087582fcd675b53ad384d31ae1117b7cb34464fa
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:48:44 +0100

    if true simplification

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -98,8 +98,6 @@ class QualityTracker:
                 if True:
                     if item.quality > 0:
                         item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
