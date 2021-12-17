307b0d7f20c4256965c9eb84011f60a3cdb71ac3
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:42:58 +0100

    Duplicate handle_backstage_pass and handle_other()

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -65,7 +65,37 @@ class QualityTracker:
         if self.is_legendary():
             return
 
-        self.handle_other()
+        if self.is_backstage_pass():
+            self.handle_backstage_pass()
+        else:
+            self.handle_other()
+
+    def handle_backstage_pass(self):
+        item = self.item
+        if not self.increases_over_time() and not self.is_backstage_pass():
+            if item.quality > 0:
+                item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if self.is_backstage_pass():
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if not self.increases_over_time():
+                if not self.is_backstage_pass():
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
 
     def handle_other(self):
         item = self.item
