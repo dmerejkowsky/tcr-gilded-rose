bfcd36fb7760ae6760ed39eabb2cd8587cf6beb4
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 20:55:43 +0100

    And an other test

--- python/test_increasing.py
+++ python/test_increasing.py
@@ -6,3 +6,10 @@ def test_increase_by_one_before_selling_time():
     strategy = Increasing(item)
     strategy.update_quality()
     assert item.quality == 11
+
+
+def test_increase_twice_as_fast_after_selling_time():
+    item = Item(name="Cheese", quality=10, sell_in=-1)
+    strategy = Increasing(item)
+    strategy.update_quality()
+    assert item.quality == 12
