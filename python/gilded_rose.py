7009693e08e6f833f40c294f4f1396b0a16576b0
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:41:35 +0100

    Handle 'if True' blocks

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -71,8 +71,7 @@ class QualityTracker:
         item = self.item
         if not self.increases_over_time() and not self.is_backstage_pass():
             if item.quality > 0:
-                if True:
-                    item.quality = item.quality - 1
+                item.quality = item.quality - 1
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
@@ -83,14 +82,12 @@ class QualityTracker:
                     if item.sell_in < 6:
                         if item.quality < 50:
                             item.quality = item.quality + 1
-        if True:
-            item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if not self.increases_over_time():
                 if not self.is_backstage_pass():
                     if item.quality > 0:
-                        if True:
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                 else:
                     item.quality = item.quality - item.quality
             else:
