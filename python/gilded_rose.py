470aeb4b86be58aea640384a85f36f44cabab276
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:56:31 +0100

    ditto

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -100,18 +100,12 @@ class QualityTracker:
 
     def handle_other(self):
         item = self.item
-        if True:
-            if item.quality > 0:
-                item.quality -= 1
-        else:
-            pass
+        if item.quality > 0:
+            item.quality -= 1
         item.sell_in -= 1
         if item.sell_in < 0:
-            if True:
-                if item.quality > 0:
-                    item.quality -= 1
-            else:
-                pass
+            if item.quality > 0:
+                item.quality -= 1
 
 
 if __name__ == "__main__":
