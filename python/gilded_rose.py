6d56ac0166c7c86651d06883cb2e0dc5c56193e8
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:45:18 +0100

    if False: x ;  else y ->  y

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -72,18 +72,14 @@ class QualityTracker:
 
     def handle_backstage_pass(self):
         item = self.item
-        if False:
-            if item.quality > 0:
-                item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if not self.increases_over_time():
