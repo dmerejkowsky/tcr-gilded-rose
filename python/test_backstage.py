32ea680a6d07a39b533f01d6e2f04371eb5435eb
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 18:02:00 +0100

    more tests

--- python/test_backstage.py
+++ python/test_backstage.py
@@ -29,8 +29,15 @@ def test_out_of_date():
     assert item.quality == 0
 
 
-def test_does_not_go_above_fifty():
+def test_does_not_go_above_fifty_when_more_than_ten():
     item = Item(name="Backstage pass", quality=49, sell_in=20)
+    strategy = BackstagePass(item)
+    strategy.update_quality()
+    assert item.quality == 50
+
+
+def test_does_not_go_above_fifty_when_less_than_ten():
+    item = Item(name="Backstage pass", quality=49, sell_in=8)
     strategy = BackstagePass(item)
     strategy.update_quality()
     assert item.quality == 50
