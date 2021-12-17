85e5d34c4d81fbab71d9590ca932d61888957694
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 17:18:42 +0100

    ditto

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -97,8 +97,8 @@ class BackstagePass(Strategy):
                 if self.get_quality() < 50:
                     self.update_quality(+1)
             if item.sell_in < 6:
-                if item.quality < 50:
-                    item.quality += 1
+                if self.get_quality() < 50:
+                    self.update_quality(+1)
         item.sell_in -= 1
         if item.sell_in < 0:
             item.quality = 0
