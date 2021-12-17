9e9a95deee221ff39f7614b2cef5f1b2aac68a32
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 17:20:04 +0100

    ditto

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -121,12 +121,12 @@ class IncreasesOverTime(Strategy):
 class Default(Strategy):
     def run(self):
         item = self.item
-        if item.quality > 0:
-            item.quality -= 1
+        if self.get_quality() > 0:
+            self.update_quality(-1)
         item.sell_in -= 1
         if item.sell_in < 0:
-            if item.quality > 0:
-                item.quality -= 1
+            if self.get_quality() > 0:
+                self.update_quality(-1)
 
 
 if __name__ == "__main__":
