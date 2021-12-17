15f87cc8ee68dd1939558a1d2be2d9178b8fa702
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:54:58 +0100

    if False simplification

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -92,16 +92,14 @@ class QualityTracker:
     def handle_increase_over_time(self):
         item = self.item
         if False:
-            if item.quality > 0:
-                item.quality -= 1
+            pass
         else:
             if item.quality < 50:
                 item.quality += 1
         item.sell_in -= 1
         if item.sell_in < 0:
             if False:
-                if item.quality > 0:
-                    item.quality -= 1
+                pass
             else:
                 if item.quality < 50:
                     item.quality += 1
