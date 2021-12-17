92617ef7e1f81188e2c8c81e6729b5b92af8e8cd
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:46:45 +0100

    x = x - x -> x = 0

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -82,7 +82,7 @@ class QualityTracker:
                     item.quality = item.quality + 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
-            item.quality = item.quality - item.quality
+            item.quality = 0
 
     def handle_other(self):
         item = self.item
