36eb9fa79d6985e17be60faef04722117aa7b358
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:54:44 +0100

    not True -> False

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -91,7 +91,7 @@ class QualityTracker:
 
     def handle_increase_over_time(self):
         item = self.item
-        if not True:
+        if False:
             if item.quality > 0:
                 item.quality -= 1
         else:
@@ -99,7 +99,7 @@ class QualityTracker:
                 item.quality += 1
         item.sell_in -= 1
         if item.sell_in < 0:
-            if not True:
+            if False:
                 if item.quality > 0:
                     item.quality -= 1
             else:
