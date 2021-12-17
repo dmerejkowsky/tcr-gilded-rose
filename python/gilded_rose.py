6629b326ab1af2f6ec2d903808c32ee0defb739d
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:49:43 +0100

    x = x + 1 -> x += 1

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -73,14 +73,14 @@ class QualityTracker:
     def handle_backstage_pass(self):
         item = self.item
         if item.quality < 50:
-            item.quality = item.quality + 1
+            item.quality += 1
             if item.sell_in < 11:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
             if item.sell_in < 6:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
+                    item.quality += 1
+        item.sell_in -= 1
         if item.sell_in < 0:
             item.quality = 0
 
