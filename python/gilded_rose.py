4165abb44333e7bffdc7d1009fa467be3b22b8ef
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:51:47 +0100

    ditto

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -96,7 +96,7 @@ class QualityTracker:
         if item.sell_in < 0:
             if not self.increases_over_time():
                 if item.quality > 0:
-                    item.quality = item.quality - 1
+                    item.quality -= 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
