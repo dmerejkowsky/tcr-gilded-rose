330d6a7a323199d417fa72b0ed63678e319393c0
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:54:02 +0100

    Re-duplicate

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -70,7 +70,7 @@ class QualityTracker:
             return
 
         if self.increases_over_time():
-            self.handle_other()
+            self.handle_increase_over_time()
             return
 
         self.handle_other()
@@ -88,6 +88,23 @@ class QualityTracker:
         item.sell_in -= 1
         if item.sell_in < 0:
             item.quality = 0
+
+    def handle_increase_over_time(self):
+        item = self.item
+        if not self.increases_over_time():
+            if item.quality > 0:
+                item.quality -= 1
+        else:
+            if item.quality < 50:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            if not self.increases_over_time():
+                if item.quality > 0:
+                    item.quality -= 1
+            else:
+                if item.quality < 50:
+                    item.quality += 1
 
     def handle_other(self):
         item = self.item
