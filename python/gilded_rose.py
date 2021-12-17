aead0a194b26d557b1701cd583b8d09aab84b527
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 16:51:20 +0100

    x = x + 1 -> x += 1

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -91,7 +91,7 @@ class QualityTracker:
                 item.quality -= 1
         else:
             if item.quality < 50:
-                item.quality = item.quality + 1
+                item.quality += 1
         item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if not self.increases_over_time():
