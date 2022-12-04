4b496301b81316a4cf003be76a56d8beab23d5eb
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-12-04 12:00:19 +0100

    Item is now a dataclass

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,14 +1,12 @@
 from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
 
 
+@dataclass
 class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    name: str
+    sell_in: int
+    quality: int
 
 
 def main():
